dd75d7961527a99434f07806a2c3d19490e99258
Samuel Wangalibo <samuelwangalibo@yahoo.com>
2023-06-16 00:01:06 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to standard error 
- *
+ * main -  Entry point
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19"\n, sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
+/**
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
