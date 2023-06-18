23d89f26e085a0d9b69137f501ad03747bdf4c62
Samuel Wangalibo <samuelwangalibo@yahoo.com>
2023-06-18 06:51:23 -0700

    0 Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /**
   * main - Entry point
+  *
   * Return: always 0 (success) 
   */
 
@@ -13,11 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
