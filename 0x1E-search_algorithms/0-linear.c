e3780a327617ebba93a5d3fa9b7746c537d7cd23
Franc6220 <samuelwangalibo@yahoo.com>
2024-05-10 07:18:05 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,24 @@
 #include "search_algos.h"
 
+
 int linear_search(int *array, size_t size, int value)
 {
-	for (int i = 0; i < size; i++)
+	if (array == NULL)
 		{
-			if (arr[i] == value)
+			return -1;
+		}
+	// Iterate through the array
+	for (i = 0; i < size; i++)
+		{
+			// Print value being compared
+			printf("Found %d at index: %d\n\n", value, linear_search(array[i], size, value);
+
+			// Compare the value
+			if (array[i] == value)
 				{
 					return i; // Return index if value is found
 				}
 		}
-	return -1; // Return -1 if value not found
-}	
+	// Value not found
+	return -1;
+}
